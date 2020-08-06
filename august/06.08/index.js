5c994791ba2042d9c631be6d29c7fe7157eb057b
Irena <irenejpopova6@gmail.com>
2020-08-06 09:42:01 +0200

    middleware

--- august/06.08/index.js
+++ august/06.08/index.js
@@ -2,14 +2,16 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(logger);
+app.use(logger); // first execute logger and then
 
-app.get("/", (req, res) => {
+// we have access to  next in any route handling, but no need to do it
+// the route handling is the : "/" - the slash
+app.get("/", (req, res, next) => {
   console.log(`Home`);
   res.send(`Home`);
 });
-
-app.get("/about", (req, res) => {
+// route handling 
+app.get("/about", userLogged,(req, res) => {
   console.log(`about`);
   res.send(`Hey`);
 });
@@ -20,7 +22,15 @@ function logger(req, res, next) {
   //return;// if you want to return you need return, 
   //console.log("log 2");
 }
-
+function userLogged(req, res, next) {
+  if (req.query.name === 'Irene') {
+    req.userName = "Irene";
+    next();
+  } else {
+    res.send("Login or register");
+  }
+}
+// route handling
 app.listen(PORT, () => {
   console.log(`server listen on http://localhost:${PORT}`);
 });
