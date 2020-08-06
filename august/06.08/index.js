b8d3e4fa2570f7a987dd135c102598bbe3314ce2
Irena <irenejpopova6@gmail.com>
2020-08-06 09:18:31 +0200

    midleware

--- august/06.08/index.js
+++ august/06.08/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use(logger);
 
 app.get("/", (req, res) => {
   console.log(`Home`);
@@ -12,6 +13,13 @@ app.get("/about", (req, res) => {
   console.log(`about`);
   res.send(`Hey`);
 });
+
+function logger(req, res, next) {
+  console.log("log 1");
+  next(); // next is not return
+  //return;// if you want to return you need return, 
+  //console.log("log 2");
+}
 
 app.listen(PORT, () => {
   console.log(`server listen on http://localhost:${PORT}`);
