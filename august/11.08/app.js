25086dc1a5bf2abb27e902e166a927aaada95f97
Irena <irenejpopova6@gmail.com>
2020-08-11 09:29:30 +0200

    server

--- august/11.08/app.js
+++ august/11.08/app.js
@@ -1,3 +1,29 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+
+//const userRouter = require("./router/users");
+// const bookRouter = require("./router/");
+
+// app.use("/users", userRouter);
+// app.use
+
+app.use((req, res, next1) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error)
+
+});
+app.use((error, req, res, next) => {
+  req.status(error.status || 500)
+  // error is an object
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+
+
+module.export = app;
