a88b5c131985f47108b015055864caa3a96f7c55
Irena <irenejpopova6@gmail.com>
2020-08-17 09:28:49 +0200

    REST API 🔥

--- august/17.08/app.js
+++ august/17.08/app.js
@@ -7,5 +7,15 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
+// mongoDB 
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  .then(console.log('DB is connected'))
+      .catch(error)=> {
+    console.log(`There was a problem ${error.message}`);
+  });
 module.exports = app;
