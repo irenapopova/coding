3786cb9e626e4e94deb09f176b094b87e3fa7b11
Irena <irenejpopova6@gmail.com>
2020-08-19 17:30:50 +0200

    small fixes

--- august/18.08 /server.js
+++ august/18.08 /server.js
@@ -2,6 +2,15 @@
 // But first REST API REpresentational State Transfer
 // new version
 const http = require("http");
+
+//  loading dotenv lib
+const dotenv = require("dotenv");
+
+//loading configs from config.env
+dotenv.config({
+  path: './config.env',
+})
+
 const app = require("./app");
 
 const PORT = process.env.PORT;
