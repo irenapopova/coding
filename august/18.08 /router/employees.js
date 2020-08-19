3d360ff8aee4109fe1738f92200dc8a9ace62d5f
Irena <irenejpopova6@gmail.com>
2020-08-19 10:18:38 +0200

    REST API 🔥

--- august/18.08 /router/employees.js
+++ august/18.08 /router/employees.js
@@ -4,29 +4,64 @@ const EmployeesData = require("../model/employeesModel")
 // get all employee
 // url http://localhost:3000/employees 
 // start a callback with async
+const {
+  getEmployee,
+  getAdd,
+  getAllEmployee,
+  addNewEmployee,
+  getOneEmployee,
+  updateOneEmployee,
+  deleteOneEmployee,
+  updateAllEmployeeData,
+  updateManyEmployees,
+} = require("../controllers/employeeController");
+
+// root rout
+router.route("/").get(getAllEmployee).post(addNewEmployee);
+// route with name value
+router
+  .route("/:name")
+  .get(getEmployee, getOneEmployee)
+  .patch(getEmployee, updateOneEmployee)
+  .put(getEmployee, updateAllEmployeeData)
+  .delete(getEmployee, deleteOneEmployee);
+
+
+// GET  http://localhost:3000/employees/ -->  get all employees
+// POST http://localhost:3000/employees/ -->  add employee
+// GET http://localhost:3000/employees/:name -->  get employee by name
+// PATCH http://localhost:3000/employees/:name -->  update employee by name
+// UPDATE aka PUT http://localhost:3000/employees/:name -->  update employee by name
+// DELETE http://localhost:3000/employees/:name -->  delete employee by name
+
+
+// GET MANY EMPLOYEES BY ADDRESS
 router.get("/".async(req, res) => {
   try{
     const employees = await EmployeesData.find(); // EmployeesData is my Schema
     // passing the status
     res.status(200).json(employees)
   } catch(err) {
-    res.status(500).json{message:error.message}
+    res.status(500).json{ message: error.message }
   }
 
 });
+// ======================end=================================//
+// Update many employees by Address 
+router.put("/update/:add", getAdd, updateManyEmployees);
 
 // add employees 
-router.post("/". async (req,res)=>{
-const employee = now EmployeesData({
-  name: req.body.name,
+router.post("/".async(req, res)=> {
+  const employee = now EmployeesData({
+    name: req.body.name,
   age: req.body.age,
   address: req.body.address,
 });
 
-try{
-const newEmployee = await employee.save();
-res.status(201).json(newEmployee);
-}catch(err{
+try {
+  const newEmployee = await employee.save();
+  res.status(201).json(newEmployee);
+} catch (err{
   res.status(400).json({
     message: err.message,
   });
@@ -36,38 +71,26 @@ res.status(201).json(newEmployee);
 });
 
 // update one employee
-route.patch("/name", getEmployee, async (req,res)=>{
-//res.send(
+route.patch("/name", getEmployee, async (req, res) => {
+  //res.send(
   // checking from the req body if 
-console.log(req.body);
+  console.log(req.body);
   // here we assigning the age
   res.employee.age
 )
 //getting data from the body 
-if (req.body.add !=null){ 
-res.employee.add = req.body.add;
+if (req.body.add != null) {
+  res.employee.add = req.body.add;
 }
 })
 
-// delete an employee
+/* // delete an employee
 router.delete
+ */
 
-router
-  .router("/:name")
-  // i am handlig 
-  // get employee is my middleware
-  .get(getEmployees, getOneEmployee)
-  .patch(getEmployee, updateOneEmployee)
-  .delete(getEmployee, deleteOneEmployee);
-// through the Schema is the connection
+/* // through the Schema is the connection
 const EmployeesData = require("../module/employeesModule");
-const { router, route } = require("../app");
+const { router, route } = require("../app"); */
 
-// GET  http://localhost:3000/employees/ -->  get all employees
-// POST http://localhost:3000/employees/ -->  add employee
-// GET http://localhost:3000/employees/:name -->  get employee by name
-// PATCH http://localhost:3000/employees/:name -->  update employee by name
-// UPDATE aka PUT http://localhost:3000/employees/:name -->  update employee by name
-// DELETE http://localhost:3000/employees/:name -->  delete employee by name
 
 module.exports = router;
