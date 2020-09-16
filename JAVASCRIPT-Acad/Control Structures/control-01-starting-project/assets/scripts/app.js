943e5c3725c15b01b67462d3f1182fffe65a1265
Irena <irenejpopova6@gmail.com>
2020-09-16 15:18:55 +0200

    updates

--- JAVASCRIPT-Acad/Control Structures/control-01-starting-project/assets/scripts/app.js
+++ JAVASCRIPT-Acad/Control Structures/control-01-starting-project/assets/scripts/app.js
@@ -29,20 +29,44 @@ function writeToLog(
   console.log(logEntries);
 }
 
-function add() {
+// adding a new function calculateResult and if-statement
+
+function calculateResult(calculationType) {
   const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  let mathOperator;
+  // the () parentheses takes the condition & the {} takes the body that should be executed
+  //if the condition is met
+  if (calculationType === 'ADD') {
+    currentResult += enteredNumber;
+    mathOperator = '+';
+  } else {
+    currentResult -= enteredNumber;
+    mathOperator = '-';
+  }
+
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+
+}
+// in functions: add, subtract, multiply & divide code duplicate, and I start with enteredNumber
+// the only information that is different is the operator that is used and the description of the operation, the other parameters are always the same 
+function add() {
+  calculateResult('ADD');
+  /* const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult += enteredNumber;
   createAndWriteOutput('+', initialResult, enteredNumber);
-  writeToLog('ADD', initialResult, enteredNumber, currentResult);
+  writeToLog('ADD', initialResult, enteredNumber, currentResult); // same parameters*/
 }
 
 function subtract() {
-  const enteredNumber = getUserNumberInput();
+  calculateResult('SUBTRACT');
+  /* const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;
   currentResult -= enteredNumber;
   createAndWriteOutput('-', initialResult, enteredNumber);
-  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+  writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult); */ // same parameters
 }
 
 function multiply() {
@@ -50,7 +74,7 @@ function multiply() {
   const initialResult = currentResult;
   currentResult *= enteredNumber;
   createAndWriteOutput('*', initialResult, enteredNumber);
-  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+  writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult); // same parameters
 }
 
 function divide() {
--- JAVASCRIPT-Acad/Control Structures/control-03-monster-killer-starting-project/assets/scripts/vendor.js
+++ JAVASCRIPT-Acad/Control Structures/control-03-monster-killer-starting-project/assets/scripts/vendor.js
@@ -7,17 +7,18 @@ const strongAttackBtn = document.getElementById('strong-attack-btn');
 const healBtn = document.getElementById('heal-btn');
 const logBtn = document.getElementById('log-btn');
 
-function adjustHealthBars(maxLife) {
-  monsterHealthBar.max = maxLife;
-  monsterHealthBar.value = maxLife;
-  playerHealthBar.max = maxLife;
-  playerHealthBar.value = maxLife;
+function adjustHealthBars(irieLife) {
+  monsterHealthBar.max = irieLife;
+  monsterHealthBar.value = irieLife;
+  playerHealthBar.max = irieLife;
+  playerHealthBar.value = irieLife;
 }
 
 function dealMonsterDamage(damage) {
+  // calculates the random amount of damage
   const dealtDamage = Math.random() * damage;
   monsterHealthBar.value = +monsterHealthBar.value - dealtDamage;
-  return dealtDamage;
+  return dealtDamage; // returns the calculated damage
 }
 
 function dealPlayerDamage(damage) {
