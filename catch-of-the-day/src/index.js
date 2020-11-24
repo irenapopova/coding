81b020355baaf752d1a06d629f3c5d86de650af6
Irena <irenejpopova6@gmail.com>
2020-11-24 09:48:32 +0100

    updates

---
+++ catch-of-the-day/src/index.js
@@ -0,0 +1,13 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+
+//class StorePicker extends React.Component {
+//render() {
+//    return <p>Hello!!</p>
+// }
+//}
+
+render(<StorePicker />, document.querySelector('#main'));
