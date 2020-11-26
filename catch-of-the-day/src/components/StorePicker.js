0d67b9a1a4cfcc78b3acfb8489c7c6f893c9ae0d
Irena <irenejpopova6@gmail.com>
2020-11-26 17:26:44 +0100

    react store

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
   render() {
-    return <p>I am the store Picker</p>
+    return (
+
+      < Fragment >
+        {/** wrap a sibling element in a React.Fragment tag */}
+        <p>Fish!</p>
+        <form className="store-selector">
+          <h2>Please enter a store</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store ➡️ </button>
+        </form >
+      </ Fragment >
+    )
   }
 }
 
