3e839291205f2b2d08494aa3a80bda27d21df162
Irena <irenejpopova6@gmail.com>
2020-12-10 09:19:05 +0100

    react

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -4,11 +4,21 @@ class Header extends React.Component {
   render() {
     return (
       <header className="top">
-        <h1>Catch o the Day</h1>
+        <h1>Catch
+        <span className="ofThe">
+            <span className="of">Of</span>
+            <span className="the">The</span>
+          </span>
+        Day</h1>
         <h3 className="tagline">
-          <span>Fresh Daily</span>
+          <span>Fresh Seafood Market </span>
+
         </h3>
-      </header>
+        <h2 className="tagline">
+          <span>Inventory</span>
+        </h2>
+
+      </header >
 
     );
   }
