9a59a35dcb7d4c6e5f2296aa9f97c4ac9a00c9a0
Irena <irenejpopova6@gmail.com>
2021-01-15 09:54:44 +0100

    updates

--- Browser-Live-Coding/Node.JS/Learn-Node/dang-thats-delicious/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const storeController = require('../controllers/storeController');
-
-// 
-router.get('/', storeController.mmyMiddleWare, storeController.homePage);
-
-module.exports = router;
