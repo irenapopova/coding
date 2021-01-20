53cee906ab1e91550dc48960ecafd4f2c85eb7d2
Irena <irenejpopova6@gmail.com>
2021-01-20 15:05:34 +0100

    connection with mysql database

--- Browser-Live-Coding/Node.JS/09-record-shop-live-p/app.js
+++ Browser-Live-Coding/Node.JS/09-record-shop-live-p/app.js
@@ -1,4 +1,8 @@
+
+
 /**External Modules */
+/* 'use strict'; */
+const mysql = require('mysql');
 const express = require("express")
 /* const logger = require("morgan") */
 const morgan = require("morgan")
@@ -8,12 +12,38 @@ const recordsRoute = require("./routes/recordsRoute")
 /**INIT (INIT/ creating  MY server(app is my server now*/
 const app = express()
 
+// connection with MySql Database
+const port = process.env.PORT || 3000;
+const responseStr = "MySql Data:";
+
+
+let mysqlHost = process.env.MYSQL_HOST || 'localhost';
+let mysqlPort = process.env.MYSQL_PORT || '3306';
+let mysqlUser = process.env.MYSQL_USER || 'root';
+let mysqlPass = process.env.MYSQL_PASS || 'root';
+let mysqlDB = process.env.MYSQL_DB || 'node_db';
+
+
+let connectionOptions = {
+  host: mysqlHost,
+  port: mysqlPort,
+  user: mysqlUser,
+  password: mysqlPass,
+  database: mysqlDB
+};
+
+console.log('MySql Connection config:');
+console.log(connectionOptions);
+
+connection.connect();
+
+
 /**In express all the controllers are Middlewares */
 /**USE MIDDLEWARE */
 /*app.use(here specify middleware)*/
 
 /* app.use(logger("combined")) */
-app.use(logger("dev"))
+app.use(morgan("dev"))
 
 
 /**ROUTES */
