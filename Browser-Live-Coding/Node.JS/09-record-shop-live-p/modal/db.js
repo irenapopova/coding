646dbdf441baa23624da702cf00e3cf42a8bdb32
Irena <irenejpopova6@gmail.com>
2021-01-20 15:07:12 +0100

    typo fixed

---
+++ Browser-Live-Coding/Node.JS/09-record-shop-live-p/modal/db.js
@@ -0,0 +1,12 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('./modal/db.json')
+const db = low(adapter)
+
+// Set some defaults
+/* db.defaults({ records: [] })
+  .write() */
+
+
+module.exports = db
