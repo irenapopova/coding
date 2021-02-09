652ccf34bcf77718f9e36af59525a4aff03d81d3
Irena <irenejpopova6@gmail.com>
2021-02-09 13:13:30 +0100

    created refs

--- Browser-Live-Coding/Node.JS/09-record-shop-live-ProjectV/controllers/ordersController.js
+++ Browser-Live-Coding/Node.JS/09-record-shop-live-ProjectV/controllers/ordersController.js
@@ -1,49 +1,70 @@
-const db = require("../model/db")
+const OrderData = require("../model/orderModel")
 
-exports.getAllOrders = (req, res, next) => {
-  //getting/reading all Orders from db.json
-  //db.get("Orders")=> target this property in db.json
-  // .value() that means get value of that property
-  let allOrders = db.get("orders").value()
-  res.send({ allOrders })
+exports.getAllOrders = async (req, res, next) => {
+  //getting/reading all orders from mongoDB
+  try {
+    let allOrders = await OrderData.find().populate("records", "-_id -__v").populate("user", "-password -_id -__v").select("-__v");
+    res.status(200).send({ allOrders });
+  } catch (err) {
+    next(err);
+  }
 }
 
-exports.postAddNewOrder = (req, res, next) => {
+exports.postAddNewOrder = async (req, res, next) => {
   console.log(req.body)
-  //add Order into db.json
-  db.get("orders").push(req.body).last().assign({ id: new Date().getTime().toString() }).write()
-  res.send("NEW Order added into database")
+  //adding new Order into mongoDB
+  try {
+    const order = new OrderData(req.body);
+    await order.save(); //store data into database
+    res.status(200).send({ order });
+  } catch (err) {
+    console.log(err.message);
+    /*  res.status(404).send({err:err.message}) */
+    next(err);
+  }
 }
 
-exports.putUpdateOrder = (req, res, next) => {
-  const { id } = req.params
-  //selector/pointer //finding that Order //update properties //store changes
-  db.get("orders").find({ id }).assign(req.body).write()
-
-  res.send("Order updated")
-}
-
-exports.deleteSingleOrder = (req, res, next) => {
-  const { id } = req.params
-  //selector/pointer //finding that Order //deleting Order //store changes
-  let Order = db.get("orders").find({ id }).value()
-  if (Order) {
-    db.get("orders").remove({ id }).write()
-    res.send("Order deleted")
-  } else {
-    let error = new Error("no such Order found in database")
-    error.status = 404
-    next(error)
+exports.putUpdateOrder = async (req, res, next) => {
+  const { id } = req.params;
+  //finding existing order with that id in database and update
+  try {
+    const updatedOrder = await OrderData.findByIdAndUpdate(id, req.body, { new: true })
+    res.status(200).send({ updatedOrder })
+  } catch (err) {
+    next(err);
   }
 
 }
 
-exports.getSingleOrder = (req, res, next) => {
+exports.deleteSingleOrder = async (req, res, next) => {
   const { id } = req.params
-  let singleOrder = db.get("orders").find({ id }).value()
-  /*  res.send({singleOrder}) */
-  res.status(200).json({ success: true, Order: singleOrder })
-  /*  res.status(404).json({success:false,message: "we couldn't find Order with that specified ID"}) */
+  //finding existing record with that id in database and delete
+  try {
+    const orderDeleted = await OrderData.findByIdAndRemove(id)
+    if (orderDeleted) {
+      res.status(200).send({ orderDeleted })
+    } else {
+      res.status(404).send("Already Deleted that record")
+    }
+  }
+  catch (err) {
+    next(err)
+  }
+
+}
+
+exports.getSingleOrder = async (req, res, next) => {
+  const { id } = req.params
+  //get/read single order from mongodb
+  try {
+    const order = await OrderData.findById(id).select("-_id -__v")
+    if (order) {
+      res.status(200).send({ order })
+    }
+    else {
+      res.status(404).send("No such record found with that Id")
+    }
+  } catch (err) { next(err) }
 }
 
 //CRUD operation
