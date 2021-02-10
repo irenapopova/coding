aa6f6790e67f1e1d43be32307ccf86fdd13c7dd4
Irena <irenejpopova6@gmail.com>
2021-02-10 09:07:46 +0100

    validation

--- Browser-Live-Coding/Node.JS/09-record-shop-live-ProjectV/controllers/recordsController.js
+++ Browser-Live-Coding/Node.JS/09-record-shop-live-ProjectV/controllers/recordsController.js
@@ -1,70 +1,71 @@
-const db = require("../model/db")
-const RecordData = require("../model/recordModel")
 
+const RecordData = require("../model/recordModel");
 exports.getAllRecords = async (req, res, next) => {
-  //getting/reading all records from db.json
-  //db.get("records")=> target this property in db.json
-  // .value() that means get value of that property
-
+  //getting/reading all records from mongoDB
   try {
     let allRecords = await RecordData.find();
     res.status(200).send({ allRecords });
   } catch (err) {
-    let error = new Error(err.message)
-    next(error)
+    next(err);
   }
 };
 
 exports.postAddNewRecord = async (req, res, next) => {
-  console.log(req.body)
+  console.log(req.body);
+  //adding new Record into mongoDB
   try {
-    const record = new RecordData(req.body)
-    await record.save() // store data in database
-    res.status(200).send({ record })
+    const record = new RecordData(req.body);
+    await record.save(); //store data into database
+    res.status(200).send({ record });
   } catch (err) {
-    const error = new Error(err.message)
-    next(error) //take this error and go to the next code
+    console.log(err.message);
+    /*  res.status(404).send({err:err.message}) */
+    next(err);
   }
+};
 
-  //add Record into db.json
-  db.get("records")
-    .push(req.body)
-    .last()
-    .assign({ id: new Date().getTime().toString() })
-    .write()
-  res.send("NEW record added into database")
-}
-
-exports.putUpdateRecord = (req, res, next) => {
-  const { id } = req.params
-  //selector/pointer //finding that record //update properties //store changes
-  db.get("records").find({ id }).assign(req.body).write()
-
-  res.send("record updated")
-}
-
-exports.deleteSingleRecord = (req, res, next) => {
-  const { id } = req.params
-  //selector/pointer //finding that record //deleting record //store changes
-  let record = db.get("records").find({ id }).value()
-  if (record) {
-    db.get("records").remove({ id }).write()
-    res.send("record deleted")
-  } else {
-    let error = new Error("no such record found in database")
-    error.status = 404
-    next(error)
+exports.putUpdateRecord = async (req, res, next) => {
+  const { id } = req.params;
+  //finding existing record with that id in database and update
+  try {
+    const updatedRecord = await RecordData.findByIdAndUpdate(id, req.body, { new: true })
+    res.status(200).send({ updatedRecord })
+  } catch (err) {
+    next(err);
   }
+};
 
-}
+exports.deleteSingleRecord = async (req, res, next) => {
+  const { id } = req.params;
+  //finding existing record with that id in database and delete
+  try {
+    const recordDeleted = await RecordData.findByIdAndRemove(id)
+    if (recordDeleted) {
+      res.status(200).send({ recordDeleted })
+    } else {
+      res.status(404).send("Already Deleted that record")
+    }
 
-exports.getSingleRecord = (req, res, next) => {
-  const { id } = req.params
-  let singleRecord = db.get("records").find({ id }).value()
-  /*  res.send({singleRecord}) */
-  res.status(200).json({ success: true, record: singleRecord })
-  /*  res.status(404).json({success:false,message: "we couldn't find record with that specified ID"}) */
-}
+  }
+  catch (err) {
+    next(err)
+  }
+};
+
+exports.getSingleRecord = async (req, res, next) => {
+  const { id } = req.params;
+  //get/read single record from mongodb
+  try {
+    const record = await RecordData.findById(id).select("-_id -__v")
+    if (record) {
+      res.status(200).send({ record })
+    }
+    else {
+      res.status(404).send("No such record found with that Id")
+    }
+  } catch (err) { next(err) }
+
+};
 
 //CRUD operation
 //RESTful API  representational state transfer
