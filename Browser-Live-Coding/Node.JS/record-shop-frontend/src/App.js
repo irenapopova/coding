d7e674c531d9edb4cc045cf44dbbd959345e2f1e
Irena <irenejpopova6@gmail.com>
2021-02-18 08:23:09 +0100

    adding local Data

--- Browser-Live-Coding/Node.JS/record-shop-frontend/src/App.js
+++ Browser-Live-Coding/Node.JS/record-shop-frontend/src/App.js
@@ -1,8 +1,12 @@
 
+import React, { createContext, useState, useEffect } from "react";
 import { Route, Link, Switch, withRouter } from "react-router-dom";
 import Home from "./components/Home";
-
-
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import NotFound from "./components/NotFound";
+import Records from "./components/Records";
 
 export const MyContext = createContext(null);
 
@@ -24,6 +28,18 @@ function App(props) {
       props.history.push("/profile");
     }
   }, []);
+
+  const logout = () => {
+    let decision = window.confirm("do you really want to logout");
+    if (decision) {
+      localStorage.clear();
+      /*   localStorage.removeItem("userdata")
+       localStorage.removeItem("token") */
+      setIsLogin(false);
+      props.history.push("/login");
+    }
+  };
+
 
   
 export default withRouter(App);
