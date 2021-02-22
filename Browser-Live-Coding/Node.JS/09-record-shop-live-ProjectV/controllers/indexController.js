9f1310ddfd68df85927ef87d493bdce0a2d72c39
Irena <irenejpopova6@gmail.com>
2021-02-22 11:29:32 +0100

    controller

--- Browser-Live-Coding/Node.JS/09-record-shop-live-ProjectV/controllers/indexController.js
+++ Browser-Live-Coding/Node.JS/09-record-shop-live-ProjectV/controllers/indexController.js
@@ -1,4 +1,14 @@
 /* NAMED EXPORT */
+const path = require("path")
+
 exports.index = (req, res, next) => {
-  res.send("hi")
+  console.log(__dirname);
+
+  res.sendFile(psth.resolve(___dirname, "..views/index.html"))
+}
+
+exports.index = (req, res, next) => {
+  console.log(__dirname);
+
+  res.sendFile(psth.resolve(___dirname, "..views/style/style.css"))
 }
